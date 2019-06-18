788f97f76e8a6e6f8351bb583394b55376d458c4
Leandro Nogueira <leandronsousa@gmail.com>
2019-06-17 23:01:43 -0300

    adsd

--- src/app/security/login/login.service.ts
+++ src/app/security/login/login.service.ts
@@ -3,7 +3,7 @@ import { User } from 'src/app/user/user.model';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Router } from '@angular/router';
-import { TISS_API } from 'src/app/app.api';
+import { TISS_API, authConfig } from 'src/app/app.api';
 import { tap, map } from 'rxjs/operators';
 import { OAuthService } from 'angular-oauth2-oidc';
 import { URLSearchParams } from 'url';
@@ -21,26 +21,37 @@ export class LoginService {
 
   public login() {
 
-    let params = new HttpParams();
-    params.append('username','leandro@gmail.com');
-    params.append('password','123456');
-    params.append('grant_type','password');
-    params.append('client_id','app-tiss-pass');
+    // this.oauthService.tokenEndpoint = 'http://localhost:8080/oauth/token';
 
-    let headers = new HttpHeaders({
-              'Content-type': 'application/x-www-form-url-encoded; charset=utf', 
-              'Authorization': 'Basic '+btoa("apptiss123456:secret"),
-              'Access-Control-Allow-Origin': 'localhost:4200', 
-              'Access-Control-Allow-Methods': 'GET, POST, DELETE, PUT', 
-              'Access-Control-Allow-Headers': 'Access-Control-Allow-Origin, Content-Type, Accept, Accept-Language, Origin, User-Agent'
-          });
-    let options = {headers: headers};
-          
-    this.http.post(`${TISS_API}/oauth/token`, params, options)
-        .pipe(map(res => res))
-        .subscribe(data => console.log(data));
+    // this.oauthService.userinfoEndpoint = 'http://localhost:8080/user/list';
 
-    //this.oauthService.initImplicitFlow();
+    // this.oauthService.clientId = 'app-tiss-pass';
+
+    // this.oauthService.scope = 'read';
+
+    // this.oauthService.dummyClientSecret = 'apptiss123456';
+
+    // this.oauthService.fetchTokenUsingPasswordFlow('leandro@gmail.com', '123456').then((resp) => {
+
+    //   // Loading data about the user
+    //   console.log(this.oauthService.loadUserProfile());
+
+    // }).then(() => {
+
+
+    //   // Using the loaded user data
+    //   let claims = this.oauthService.getIdentityClaims();
+    //   if (claims) console.log('given_name', claims);
+
+    // })
+
+
+    this.oauthService.configure(authConfig);
+    this.oauthService.loadDiscoveryDocument();
+    this.oauthService.customQueryParams = {
+      'grant_type': 'implicit'
+    }
+    this.oauthService.initImplicitFlow();
   }
 
   public logout() {
