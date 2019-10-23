f022c5951ba599dfbdae08b2c03720dfb7380bb0
Leandro Nogueira <leandronsousa@gmail.com>
2019-10-22 23:18:58 -0300

    update to angular 8

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatSidenavModule, MatToolbarModule, MatIconModule,
-         MatButtonModule, MatListModule, MatCardModule,
-         MatFormFieldModule, MatInputModule,  MatTableModule,
-         MatRadioModule, MatSelectModule, MatMenuModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [],
